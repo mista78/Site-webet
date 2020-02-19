fd9433ebf582b8aab197d50d9c7bf1faecffa0c1
Halgo <halgo@mbp-de-halgo.home>
2020-02-19 23:15:43 +0100

    init2

--- App/Component/Generateprono/index.js
+++ App/Component/Generateprono/index.js
@@ -1,6 +1,6 @@
 (function (){
-    let key = "2691616c84fe3c9072e2b1a1b205983d";
+    let key = "?apiKey=2691616c84fe3c9072e2b1a1b205983d";
     let url = "https://api.the-odds-api.com/v3/sports/";
-    let getPari = url + "?apiKey=" + key + "&sport=soccer_epl&region=uk&mkt=h2h";
-    let getSport = url + "https://api.the-odds-api.com/v3/sports/?apiKey=" + key;
+    let getPari = url + key + "&sport=soccer_epl&region=uk&mkt=h2h";
+    let getSport = url + key;
 })()
